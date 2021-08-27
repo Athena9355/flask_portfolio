d96eb4765b4225a4c17e0bb6d8958f391bb7d9da
Athena9355 <gte782a@gmail.com>
2021-08-26 18:44:42 -0700

    hi

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from flask import Flask, render_template
 # create a Flask instance
 app = Flask(__name__)
 
-
 # connects default URL to render index.html
-@app.route('/')
+@app.route('/') #this is the first page. runs the function: "def index". have to add tab below index. defined roots, roots are connected to functions
 def index():
     return render_template("index.html")
 
@@ -18,7 +17,7 @@ def kangaroos():
 
 
 @app.route('/walruses/')
-def walruses():
+def walruses():#these are the different route
     return render_template("walruses.html")
 
 
@@ -29,9 +28,12 @@ def hawkers():
 
 @app.route('/stub/')
 def stub():
-    return render_template("stub.html")
+    return render_template("stub.html") #code for the function
+
 
 
 # runs the application on the development server
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) #says "run this directly" app.run will run the server
+
+#index.html is standard
